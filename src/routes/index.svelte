9f1ba4d9944685a9a46154a72bb68f27e8876286
Melissa De Jesus <mdejesus2018@fau.edu>
2022-06-29 22:39:14 -0400

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<h1 class="text-xl font-semibold" on:click="{click}">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class= "bg-blue-400 text-white py-2 px-3 m-2 rounded" >Button</button>
+
+<svelte:head>
+    <style>
+        body{
+            background-color: bisque;
+        }
+    </style>
+</svelte:head>
+<script>
+function click(){
+    console.log("click");
+}
+</script>
