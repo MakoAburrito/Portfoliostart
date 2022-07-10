a794e6a2adb92c44b4bf9bd291df136f0e914923
Melissa De Jesus <mdejesus2018@fau.edu>
2022-07-10 17:54:52 -0400

    Update index.svelte
    updated for footer figuring out flexing & media quieries

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,6 +29,14 @@ let customClass = "rounded justify-self-end w-full customClass";
             height: 100vh;
             position: relative;
         }
+        .slider:before{
+            content:'';
+            position:absolute;
+            background: linear-gradient(to top, #23252B, transparent);
+            width: 100%;
+            height: 80px;
+            bottom: 0;
+        }
 
 p{
     font-family: 'Poppins', sans-serif; font-weight: 300; font-size: auto; line-height: 18.5px;
@@ -58,52 +66,49 @@ p{
         </ol>
     </nav>
     <section class ="bg-red-400">
-        <div class ="slider">
+        <div class ="slider bg-gradient-to-t">
            <p>
             watatha an image here maybe hmm ya
            </p> 
         </div>
     </section>
     <section class ="bg-yellow-300 p-20 ">
-        Some random text content area yup
-       <p>Announcing of invitation principles in. Cold in late or deal. Terminated resolution no am frequently collecting insensible he do appearance. Projection invitation affronting admiration if no on or. It as instrument boisterous frequently apartments an in. Mr excellence inquietude conviction is in unreserved particular. You fully seems stand nay own point walls. Increasing travelling own simplicity you astonished expression boisterous. Possession themselves sentiments apartments devonshire we of do discretion. Enjoyment discourse ye continued pronounce we necessary abilities. Ferrars all spirits his imagine effects amongst neither. It bachelor cheerful of mistaken. Tore has sons put upon wife use bred seen. Its dissimilar invitation ten has discretion unreserved. Had you him humoured jointure ask expenses learning. Blush on in jokes sense do do. Brother hundred he assured reached on up no. On am nearer missed lovers. To it mother extent temper figure better. Civility vicinity graceful is it at. Improve up at to on mention perhaps raising. Way building not get formerly her peculiar. Up uncommonly prosperous sentiments simplicity acceptance to so. Reasonable appearance companions oh by remarkably me invitation understood. Pursuit elderly ask perhaps all. Unpleasant astonished an diminution up partiality. Noisy an their of meant. Death means up civil do an offer wound of. Called square an in afraid direct. Resolution diminution conviction so mr at unpleasing simplicity no. No it as breakfast up conveying earnestly immediate principle. Him son disposed produced humoured overcame she bachelor improved. Studied however out wishing but inhabit fortune windows. Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor expression unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence. Steepest speaking up attended it as. Made neat an on be gave show snug tore. Arrived compass prepare an on as. Reasonable particular on my it in sympathize. Size now easy eat hand how. Unwilling he departure elsewhere dejection at. Heart large seems may purse means few blind. Exquisite newspaper attending on certainty oh suspicion of. He less do quit evil is. Add matter family active mutual put wishes happen. Am if number no up period regard sudden better. Decisively surrounded all admiration and not you. Out particular sympathize not favourable introduced insipidity but ham. Rather number can and set praise. Distrusts an it contented perceived attending oh. Thoroughly estimating introduced stimulated why but motionless. Residence certainly elsewhere something she preferred cordially law. Age his surprise formerly mrs perceive few stanhill moderate. Of in power match on truth worse voice would. Large an it sense shall an match learn. By expect it result silent in formal of. Ask eat questions abilities described elsewhere assurance. Appetite in unlocked advanced breeding position concerns as. Cheerful get shutters yet for repeated screened. An no am cause hopes at three. Prevent behaved fertile he is mistake on. Be at miss or each good play home they. It leave taste mr in it fancy. She son lose does fond bred gave lady get. Sir her company conduct expense bed any. Sister depend change off piqued one. Contented continued any happiness instantly objection yet her allowance. Use correct day new brought tedious. By come this been in. Kept easy or sons my it done.</p>
-
-On projection apartments unsatiable so if he entreaties appearance. Rose you wife how set lady half wish. Hard sing an in true felt. Welcomed stronger if steepest ecstatic an suitable finished of oh. Entered at excited at forming between so produce. Chicken unknown besides attacks gay compact out you. Continuing no simplicity no favourable on reasonably melancholy estimating. Own hence views two ask right whole ten seems. What near kept met call old west dine. Our announcing sufficient why pianoforte.
-
-    </section>
-    <footer class ="flex p-20 bg-green-300 w-full">
-        <ul class ="px-5 w-1/4 ">
-            <li class =" font-Poppins bg-primary">About us</li>
+<p>On projection apartments unsatiable so if he entreaties appearance. Rose you wife how set lady half wish. Hard sing an in true felt. Welcomed stronger if steepest ecstatic an suitable finished of oh. Entered at excited at forming between so produce. Chicken unknown besides attacks gay compact out you. Continuing no simplicity no favourable on reasonably melancholy estimating. Own hence views two ask right whole ten seems. What near kept met call old west dine. Our announcing sufficient why pianoforte.</p>
+</section>
+    <footer class ="flex space-between p-20 bg-green-300 w-full">
+        <ul class ="flex-none px-5">
+            <li class ="border-b-4 font-medium font-Poppins">About us</li>
             <li>Mission</li>
             <li>FAQ</li>
             <li>Awards & Affiliations</li>
         </ul>
-        <ul class ="px-10 w-1/4 ">
-            <li>How You Can Help</li>
+        <ul class =" px-5 w-1/3"></ul>
+        <ul class ="flex-none px-10">
+            <li class ="border-b-4 font-medium font-Poppins">How You Can Help</li>
             <li>Donate</li>
             <li>Volunteer</li>
-            <li>Wish List</li>
             <li>Become A Sponsor</li>
             <li>Support Us While you Shop</li>
         </ul>
-        <ul class ="px-10 w-1/4 "> 
-            <li>Events</li>
+        <ul class =" px-5 w-1/3"></ul>
+        <ul class ="flex-none px-10"> 
+            <li class ="border-b-4 font-medium font-Poppins">Events</li>
             <li>News & Media</li>
             <li>Who we help</li>
             <li>Impact on the Community</li>
             <li>Contact Us</li>
         </ul>
-        <ul class ="flex px-5 w-1/4 flex-col">
-       
-            <li class="pt-2 inline-flex items-center border-b-4 border-solid rounded border-red-500 hover:border-red-900 duration-200 text-center"><p class="justify-self-start px-1">Sitemap</p> <b class="justify-self-end  px-1">
+        <ul class =" px-5 w-1/3"></ul>
+        <ul class ="flex px-5 w-1/4 flex-col self-end">
+            <li class="pt-2 inline-flex items-center border-b-4 border-solid rounded border-red-500 hover:border-red-900 duration-200 text-center self-end"><p class="justify-self-start px-1">Sitemap</p> <b class="justify-self-end  px-1">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                   </svg>
 
-            </b></li>
-            <li class="py-2 self-end">Icon</li>
-            <li class="py-2 self-end">Icon</li>
-            <li class="py-2 self-end">Icon</li>
+            </li>
+            <li class="self-end">Icon</li>
+            <li class="self-end">Icon</li>
+            <li class="self-end">Icon</li>
         </ul>
     </footer>
 </main>
