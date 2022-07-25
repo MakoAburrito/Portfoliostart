f61d97831b9b5069cd9f1d3209e56eacb80ddb18
Melissa De Jesus <mdejesus2018@fau.edu>
2022-07-25 19:31:58 -0400

    something is being dumb lol

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,12 +23,16 @@ let customClass = "rounded justify-self-end w-full customClass";
         background-color: bisque;
     }
         .slider{
+<<<<<<< Updated upstream
             background: url("./images/photo.jpg");
+=======
+            background-image: url("./Images/Shoes2.jpg");
+>>>>>>> Stashed changes
             background-position: center;
             background-repeat: no-repeat;
             background-size: cover;
-            width: 100%;
-            height: 100vh;
+            width: 100vw;
+            height: 500px;
             position: relative;
         }
         .slider:before{
@@ -77,6 +81,7 @@ p{
            </p> 
         </div>
     </section>
+<<<<<<< Updated upstream
     <section class ="bg-yellow-300 p-20 ">
 <p>On projection apartments unsatiable so if he entreaties appearance. Rose you wife how set lady half wish. Hard sing an in true felt. Welcomed stronger if steepest ecstatic an suitable finished of oh. Entered at excited at forming between so produce. Chicken unknown besides attacks gay compact out you. Continuing no simplicity no favourable on reasonably melancholy estimating. Own hence views two ask right whole ten seems. What near kept met call old west dine. Our announcing sufficient why pianoforte.</p>
 </section>
@@ -87,6 +92,58 @@ p{
                 <a href="#" >News & Media</a>
             </p>
             <p>
+=======
+    <section class ="bg-[#23252B] p-40 text-white">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae semper quis lectus nulla at volutpat diam ut venenatis. Velit euismod in pellentesque massa placerat duis ultricies lacus. Nunc mattis enim ut tellus elementum sagittis vitae et. Turpis massa sed elementum tempus egestas sed. Euismod in pellentesque massa placerat duis ultricies lacus. Nam libero justo laoreet sit amet. Risus quis varius quam quisque id diam vel. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin gravida. Amet justo donec enim diam vulputate ut.
+    <br><br>
+        Nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi. Pharetra et ultrices neque ornare aenean euismod elementum. Aliquet lectus proin nibh nisl. Sed id semper risus in. Sit amet risus nullam eget felis eget nunc lobortis mattis. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Fermentum dui faucibus in ornare quam. Vitae turpis massa sed elementum tempus egestas sed. Enim sed faucibus turpis in. Viverra nam libero justo laoreet sit amet cursus sit. Netus et malesuada fames ac turpis egestas integer eget aliquet. Nulla aliquet porttitor lacus luctus accumsan. Turpis egestas integer eget aliquet nibh praesent tristique. Ac turpis egestas maecenas pharetra convallis posuere morbi leo. Ullamcorper malesuada proin libero nunc consequat interdum varius. Nisl suscipit adipiscing bibendum est ultricies integer quis auctor.
+    <br><br>
+    </p>
+        <aside>
+        <p>
+            Consequat ac felis donec et odio. Euismod quis viverra nibh cras pulvinar. Egestas dui id ornare arcu odio ut sem nulla pharetra. Nulla facilisi cras fermentum odio eu. Pretium nibh ipsum consequat nisl vel pretium. Dignissim diam quis enim lobortis. Orci ac auctor augue mauris augue neque gravida. Euismod lacinia at quis risus sed vulputate. Et tortor at risus viverra. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur. Odio euismod lacinia at quis risus sed vulputate.
+        <br><br>    
+            Eu tincidunt tortor aliquam nulla facilisi cras fermentum. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Enim neque volutpat ac tincidunt vitae. Nec sagittis aliquam malesuada bibendum arcu vitae. Vulputate odio ut enim blandit. Mauris rhoncus aenean vel elit. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Dictum non consectetur a erat. Vel orci porta non pulvinar neque laoreet. Pretium quam vulputate dignissim suspendisse in est ante in nibh. Quis imperdiet massa tincidunt nunc pulvinar. Donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu.
+        <br><br>    
+            Pellentesque dignissim enim sit amet venenatis urna. Sit amet consectetur adipiscing elit ut aliquam purus sit amet. Felis imperdiet proin fermentum leo vel orci porta non pulvinar. Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Egestas sed sed risus pretium quam. Non enim praesent elementum facilisis leo. Bibendum est ultricies integer quis. Dapibus ultrices in iaculis nunc sed augue lacus. Tempus iaculis urna id volutpat lacus laoreet. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.
+        </p>
+        </aside>
+    <p>
+        Consequat ac felis donec et odio. Euismod quis viverra nibh cras pulvinar. Egestas dui id ornare arcu odio ut sem nulla pharetra. Nulla facilisi cras fermentum odio eu. Pretium nibh ipsum consequat nisl vel pretium. Dignissim diam quis enim lobortis. Orci ac auctor augue mauris augue neque gravida. Euismod lacinia at quis risus sed vulputate. Et tortor at risus viverra. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur. Odio euismod lacinia at quis risus sed vulputate.
+        <br><br>    
+        Eu tincidunt tortor aliquam nulla facilisi cras fermentum. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Enim neque volutpat ac tincidunt vitae. Nec sagittis aliquam malesuada bibendum arcu vitae. Vulputate odio ut enim blandit. Mauris rhoncus aenean vel elit. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Dictum non consectetur a erat. Vel orci porta non pulvinar neque laoreet. Pretium quam vulputate dignissim suspendisse in est ante in nibh. Quis imperdiet massa tincidunt nunc pulvinar. Donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu.
+        <br><br>    
+        Pellentesque dignissim enim sit amet venenatis urna. Sit amet consectetur adipiscing elit ut aliquam purus sit amet. Felis imperdiet proin fermentum leo vel orci porta non pulvinar. Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Egestas sed sed risus pretium quam. Non enim praesent elementum facilisis leo. Bibendum est ultricies integer quis. Dapibus ultrices in iaculis nunc sed augue lacus. Tempus iaculis urna id volutpat lacus laoreet. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.
+    </p>
+    </section>
+    <footer class ="flex space-between p-20 bg-green-300 w-full">
+        <ul class ="px-5">
+            <li class ="border-b-4 font-medium font-Poppins">About us</li>
+            <li>Mission</li>
+            <li>FAQ</li>
+            <li>Awards & Affiliations</li>
+        </ul>
+        <ul class ="px-10">
+            <li class ="border-b-4 font-medium font-Poppins">How You Can Help</li>
+            <li>Donate</li>
+            <li>Volunteer</li>
+            <li>Become A Sponsor</li>
+            <li>Support Us While you Shop</li>
+        </ul>
+        <ul class ="px-10"> 
+            <li class ="border-b-4 font-medium font-Poppins">Events</li>
+            <li>News & Media</li>
+            <li>Who we help</li>
+            <li>Impact on the Community</li>
+            <li>Contact Us</li>
+        </ul>
+        <ul class ="flex px-5 w-1/4 flex-col self-end">
+            <li class="pt-2 inline-flex items-center border-b-4 border-solid rounded border-red-500 hover:border-red-900 duration-200 text-center self-end"><p class="justify-self-start px-1">Sitemap</p> <b class="justify-self-end  px-1">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                  </svg>
+>>>>>>> Stashed changes
 
                 <a href="#" class="break-after-row" >Who we help</a>
             </p>
